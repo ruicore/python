753fd94f16f4c51528ea06e5e7a4663c53188b3e
HeRuivio <super76rui@icloud.com>
2018-08-05 17:00:02 +0800

    2018-08-05 fromat to class

--- Spider/practise.py
+++ Spider/practise.py
@@ -188,47 +188,48 @@ def mongodb():
     print(result.deleted_count)
 # =============参数练习=============
 
-
-def story(**kwds):
-    return "Once upon a time there was a "\
-        "%(job)s called %(name)s." % kwds
-
-
-def power(x, y, *others):
-    if others:
-        print("Recived redundant parameters:", others)
-    return pow(x, y)
+class params():
+    def story(self,**kwds):
+        return "Once upon a time there was a "\
+            "%(job)s called %(name)s." % kwds
 
 
-def interval(start, stop=None, step=1):
-    'Imitates range() for step >0'
-    if stop is None:
-        start, stop = 0, start
-    result = []
-    i = start
-    while i < stop:
-        result.append(i)
-        i += step
-    return result
+    def power(self,x, y, *others):
+        if others:
+            print("Recived redundant parameters:", others)
+        return pow(x, y)
 
 
-def param_driver():
-    print(story(job='king', name='Gumby'))
-    print(story(name='Sir Robin', job='brave knight'))
-    params = {'job': 'language', 'name': 'Python'}
-    print(story(**params))
-    del params['job']
-    print(story(job='stoke of genius', **params))
-    print(power(2, 3))
-    print(power(3, 2))
-    power(x=2, y=3)
-    params = (5,)*2
-    print(power(*params))
-    print(power(3, 3, 'Hello,world'))
-    print(interval(10))
-    print(interval(3, 12, 4))
-    print(power(*interval(3, 7)))
+    def interval(self,start, stop=None, step=1):
+        'Imitates range() for step >0'
+        if stop is None:
+            start, stop = 0, start
+        result = []
+        i = start
+        while i < stop:
+            result.append(i)
+            i += step
+        return result
+
+
+    def param_driver(self):
+        print(self.story(job='king', name='Gumby'))
+        print(self.story(name='Sir Robin', job='brave knight'))
+        params = {'job': 'language', 'name': 'Python'}
+        print(self.story(**params))
+        del params['job']
+        print(self.story(job='stoke of genius', **params))
+        print(self.power(2, 3))
+        print(self.power(3, 2))
+        self.power(x=2, y=3)
+        params = (5,)*2
+        print(self.power(*params))
+        print(self.power(3, 3, 'Hello,world'))
+        print(self.interval(10))
+        print(self.interval(3, 12, 4))
+        print(self.power(*self.interval(3, 7)))
 
 
 if __name__ == "__main__":
-    param_driver()
+    p = params()
+    p.param_driver()
