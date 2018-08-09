6622e399e694e051afd882f16f261b8a65fc3201
HeRuivio <super76rui@icloud.com>
2018-08-09 11:50:35 +0800

    2018-08-09 正则表达式的另一种注释方式

--- Learn/templates.py
+++ Learn/templates.py
@@ -28,3 +28,13 @@ for line in fileinput.input():
 text = ''.join(lines)
 
 print(filed_part.sub(replacement, text))
+
+
+emphasis_pattern = re.compile(r'''
+        \*          # Beginning emphasis tag -- an asterisk
+        (           # Begin group for capturing phrase
+        [^\*]+      # Capture anything except asterisk
+        )           # End group
+        \*          # Ending emphasis tag
+        ''', re.VERBOSE)
+print(re.sub(emphasis_pattern,r'<em>\1</em>','Hello,*world*!'))
