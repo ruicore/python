a2a50e6e5e720e9c818297148f5ae89e72094919
HeRuivio <super76rui@icloud.com>
2019-04-12 19:50:42 +0800

    2019-04-11 头条校招

--- Learn/2019-04-11-written-examination06.py
+++ Learn/2019-04-11-written-examination06.py
@@ -2,19 +2,20 @@ import sys
 
 
 class toutiao:
-    def combine(self, string: str, operations: [[int, int]]) -> str:
-        str_list = list(string)
-        for item in operations:
-            num1, num2 = int(item[0]), int(item[1])
-            index = num1 + num2
-            for i in range(num1 + num2 - 1, num1 - 1, -1):
-                str_list.insert(index, str_list[i])
-                index += 1
-        return "".join(str_list)
-
+    def count(self, nums: [int]) -> int:
+        dp = [2 for _ in range(len(nums))]
+        nums.sort()
+        for i in range(1, len(nums)):
+            if dp[i - 1] == 0:
+                dp[i] = 2
+            elif nums[i] - nums[i - 1] > 20:
+                dp[i] = dp[i - 1] + 2
+            else:
+                dp[i] = dp[i - 1] - 1
+        return dp[-1]
 
 if __name__ == "__main__":
-    lines = [line.strip().split() for line in sys.stdin.readlines()]
+    lines = [list(map(int, line.strip().split())) for line in sys.stdin.readlines()]
     so = toutiao()
-    res = so.combine(lines[0][0], lines[2:])
+    res = so.count(lines[1])
     print(res)
