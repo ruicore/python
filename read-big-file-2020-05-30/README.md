5c7e20cea9fd5bb2427581e8c83ad125de9c2f98
何睿 <hrui835@gmail.com>
2020-05-31 07:53:31 +0800

    python batch read file

---
+++ read-big-file-2020-05-30/README.md
@@ -0,0 +1,3 @@
+# Batch Read file
+
+* batch read file, set batch to 100, and leave last word to next round to handle.
---
+++ read-big-file-2020-05-30/read_file.py
@@ -0,0 +1,30 @@
+import re
+
+
+class BatchReadFile:
+    def __init__(self):
+        self.BATCH_SIZE = 100
+
+    def _read(self, content, last_word):
+        text = re.sub(r'[^\w ]', ' ', last_word + content)  # filter out non-words and non-space
+        word_list = text.split(" ")
+        f_word_list, last_word = word_list[:-1], word_list[-1]
+
+        return list(filter(lambda x: x, f_word_list)), last_word
+
+    def process(self, file_path):
+        with open(file_path, "r") as f:
+            last_word = ''
+            while True:
+                chars = f.read(self.BATCH_SIZE)
+                if not chars:
+                    break
+                full_words, last_word = self._read(chars, last_word)
+                if full_words:
+                    print(full_words)
+
+        return
+
+
+batch_read_file = BatchReadFile()
+batch_read_file.process("large.txt")
