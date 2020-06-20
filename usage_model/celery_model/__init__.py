f7f825ad14f554831be135d143290618d5a389e9
何睿 <hrui835@gmail.com>
2020-06-20 20:01:14 +0800

    feat(celery_model): add celery
    this is a usage for celery, which handle scheduled task

--- usage_model/celery_model/__init__.py
+++ usage_model/celery_model/__init__.py
@@ -1,8 +1,6 @@
-from usage_model.celery_model import celeryconfig
 from celery import Celery
+
+from usage_model.celery_model import celeryconfig
 
 app = Celery("tasks")
 app.config_from_object(celeryconfig)
-
-if __name__ == "__main__":
-    app.start()
---
+++ usage_model/celery_model/tasks.py
@@ -0,0 +1,24 @@
+import sys
+
+from celery.utils.log import get_logger
+
+from usage_model.celery_model import app
+
+sys.path.append("/Users/herui/Desktop/python/")
+logger = get_logger(__name__)
+
+
+@app.task(bind=True)
+def add(self, a, b):
+    logger.info(f"add {a} and {b}")
+    return a + b
+
+
+@app.task
+def mul(a=1, b=1):
+    logger.info(f"mul {a} and {b}")
+    return a * b
+
+
+if __name__ == "__main__":
+    app.start()
