4e738a59d1461112f4dcbaa751e96c13ecf37025
何睿 <hrui835@gmail.com>
2020-06-21 12:07:30 +0800

    feat(db_model/__init__.py): add db conncet decorator
    this is a sample usage for init connecting database by using a decorator

---
+++ usage_model/db_model/__init__.py
@@ -0,0 +1,25 @@
+from functools import partial, wraps
+
+from databases import Database
+
+
+def init_db(func=None, *, db: Database = None):
+    if func is None:
+        if db is None:
+            raise ValueError("init db must not be None")
+        return partial(init_db, db=db)
+
+    @wraps(func)
+    async def wrapper(*args, **kwargs):
+        if not db.is_connected:
+            await db.connect()
+        try:
+            result = await func(*args, **kwargs)
+        except Exception as e:
+            return str(e)
+        finally:
+            if db.is_connected:
+                await db.disconnect()
+        return result
+
+    return wrapper
