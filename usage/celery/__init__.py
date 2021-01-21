73c7243a499d72312ccebf92d9724d5790d30ce9
何睿 <hrui835@gmail.com>
2021-01-21 13:32:46 +0800

    refactor(all): refactor file format

--- usage/celery/__init__.py
+++
@@ -1,6 +0,0 @@
-from celery import Celery
-
-from usage_model.celery_model import celeryconfig
-
-app = Celery("tasks")
-app.config_from_object(celeryconfig)
--- usage/celery/celeryconfig.py
+++
@@ -1,7 +0,0 @@
-broker_url = 'redis://localhost:6379/1'
-
-# Using the database to store task state and results.
-result_backend = 'redis://localhost:6379/1'
-
-enable_utc = False
-timezone = 'Asia/Shanghai'
